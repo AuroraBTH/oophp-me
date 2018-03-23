82679bcaf27741b618d5f6bb9ec212ac88509d1b
Niklas Andersson <nhdandersson@gmail.com>
2018-03-23 19:35:57 +0100

    Full rework of the sites layout, now uses Bootstrap.

--- view/layout/oophp/default.php
+++ view/layout/oophp/default.php
@@ -9,79 +9,58 @@ namespace Anax\View;
 // Show incoming variables and view helper functions
 //echo showEnvironment(get_defined_vars(), get_defined_functions());
 
-?><!doctype html>
+?>
+<!DOCTYPE html>
 <html>
-<head>
-    <meta charset="utf-8">
-    <title><?= $title ?? "No title" ?></title>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <head>
+        <meta charset="utf-8">
+        <title><?= $title ?? "No title" ?></title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-<?php if (isset($favicon)) : ?>
-    <link rel="icon" href="<?= $favicon ?>">
-<?php endif; ?>
+    <?php if (isset($favicon)) : ?>
+        <link rel="icon" href="<?= $favicon ?>">
+    <?php endif; ?>
 
-<?php foreach ($stylesheets as $stylesheet) : ?>
-    <link rel="stylesheet" type="text/css" href="<?= asset($stylesheet) ?>">
-<?php endforeach; ?>
+    <?php foreach ($stylesheets as $stylesheet) : ?>
+        <link rel="stylesheet" type="text/css" href="<?= asset($stylesheet) ?>">
+    <?php endforeach; ?>
 
-</head>
-<body>
+    </head>
+    <body>
 
-<!-- header -->
-<?php if (regionHasContent("header")) : ?>
-<div class="outer-wrap outer-wrap-header">
-    <div class="inner-wrap inner-wrap-header">
-        <div class="row">
-            <div class="wrap-header">
-                <?php renderRegion("header") ?>
+    <!-- header -->
+    <?php if (regionHasContent("header")) : ?>
+        <?php renderRegion("header") ?>
+    <?php endif; ?>
+
+    <!-- navbar -->
+    <?php if (regionHasContent("navbar")) : ?>
+        <?php renderRegion("navbar") ?>
+    <?php endif; ?>
+
+    <!-- main -->
+    <?php if (regionHasContent("main")) : ?>
+        <div class="main-wrap">
+            <div class="d-flex justify-content-center">
+                <main class="d-flex w-75">
+                    <?php renderRegion("main") ?>
+                </main>
             </div>
         </div>
-    </div>
-</div>
-<?php endif; ?>
+    <?php endif; ?>
 
-<!-- navbar -->
-<?php if (regionHasContent("navbar")) : ?>
-<div class="outer-wrap outer-wrap-navbar">
-    <div class="inner-wrap inner-wrap-navbar">
-        <div class="row">
-            <div class="wrap-navbar">
-                <?php renderRegion("navbar") ?>
-            </div>
-        </div>
-    </div>
-</div>
-<?php endif; ?>
-
-<!-- main -->
-<?php if (regionHasContent("main")) : ?>
-<div class="outer-wrap outer-wrap-main">
-    <div class="inner-wrap inner-wrap-main">
-        <div class="row">
-            <main class="wrap-main">
-                <?php renderRegion("main") ?>
-            </main>
-        </div>
-    </div>
-</div>
-<?php endif; ?>
-
-<!-- footer -->
-<?php if (regionHasContent("footer")) : ?>
-<div class="outer-wrap outer-wrap-footer">
-    <div class="inner-wrap inner-wrap-footer">
-        <div class="row">
-            <div class="wrap-footer">
+    <!-- footer -->
+    <?php if (regionHasContent("footer")) : ?>
+        <div class="footer">
+            <div class="d-flex justify-content-around bg-light border border-bottom-0 border-left-0 border-right-0 py-4">
                 <?php renderRegion("footer") ?>
             </div>
         </div>
-    </div>
-</div>
-<?php endif; ?>
+    <?php endif; ?>
 
-<?php foreach ($javascripts as $javascript) : ?>
-<script async src="<?= asset($javascript) ?>"></script>
-<?php endforeach; ?>
+    <?php foreach ($javascripts as $javascript) : ?>
+    <script async src="<?= asset($javascript) ?>"></script>
+    <?php endforeach; ?>
 
-</body>
+    </body>
 </html>
