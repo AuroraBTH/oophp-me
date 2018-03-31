c74d7734ed5ec5457628fce7277e536a32685dc6
Niklas Andersson <nhdandersson@gmail.com>
2018-03-31 21:37:52 +0200

    No longer has padding bottom and now has a link to Guess.

--- view/navbar/oophp/default.php
+++ view/navbar/oophp/default.php
@@ -10,7 +10,7 @@ namespace Anax\View;
 //echo showEnvironment(get_defined_vars(), get_defined_functions());
 
 ?>
-<nav class="navbar justify-content-around navbar-expand-lg navbar-light bg-light border border-right-0 border-left-0 border-top-0 mb-4">
+<nav class="navbar justify-content-around navbar-expand-lg navbar-light bg-light border border-right-0 border-left-0 border-top-0">
     <a href="<?= url("") ?>" class="navbar-brand w-25"><img class="w-100" src="http://i.imgur.com/qIp1xEh.gif" alt="name-logo"></a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
@@ -18,6 +18,7 @@ namespace Anax\View;
     <a href="<?= url("") ?>" class="nav-link text-dark">Home</a>
     <a href="<?= url("report") ?>" class="nav-link text-dark">Report</a>
     <a href="<?= url("about") ?>" class="nav-link text-dark">About</a>
+    <a href="<?= url("guess/get") ?>" class="nav-link text-dark">Guess</a>
     <a href="<?= url("play") ?>" class="nav-link text-dark">Play</a>
     <a href="<?= url("debug") ?>" class="nav-link text-dark">Debug</a>
 </nav>
